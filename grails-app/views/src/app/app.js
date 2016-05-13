d75ea5ebf1628c6eae650540d5583a46fdca365d
Zachary Klein <zak@silver-chalice.com>
2016-05-13 13:14:01 -0500

    Refactored React app to better support hot-load

--- grails-app/views/src/app/app.js
+++ grails-app/views/src/app/app.js
@@ -3,16 +3,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-require('./../styles/style.css');
+import {Hello} from './hello';
 
-class Hello extends React.Component{
-    render() {
-        return <div>
-            <h1>Hello from React!</h1>
-            <img className="center" src={require('./../images/react.png')} />
-            <span className="webpack">&nbsp;</span>
-        </div>;
-    }
-}
 
 ReactDOM.render(<Hello />, document.getElementById('app'));
