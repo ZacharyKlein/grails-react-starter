0c9213bcba482c88c690b210efb0ea80d2f31b05
Zachary Klein <zak@silver-chalice.com>
2016-05-14 00:26:15 -0500

    Check that dev server is running

--- grails-app/taglib/grails/react/starter/WebpackTagLib.groovy
+++ grails-app/taglib/grails/react/starter/WebpackTagLib.groovy
@@ -19,9 +19,24 @@ class WebpackTagLib {
     def bundle = { attrs ->
         def bundleName = attrs.bundle ?: DEFAULT_BUNDLE_NAME
 
-        if(attrs.devServer == 'true' && devServerEnabled && Environment.currentEnvironment == Environment.DEVELOPMENT)
+        if (attrs.devServer == 'true' && devServerEnabled && Environment.isDevelopmentMode() && devServerUp())
             out << "<script type='text/javascript' src='${devServerUrl}/assets/${bundleName}'></script>"
         else
             out << "${asset.javascript(src: bundleName)}"
+    }
+
+    private Boolean devServerUp() {
+        def url = new URL(devServerUrl);
+        HttpURLConnection connection =  (HttpURLConnection) url.openConnection()
+        connection.setRequestMethod("HEAD")
+
+        try {
+            connection.connect()
+            //log.info "Response Code: ${connection.responseCode}"
+            return (connection.responseCode == HttpURLConnection.HTTP_OK);
+        } catch(e) {
+            log.warn "Could not access webpack-dev-server at $devServerUrl - $e"
+            return false
+        }
     }
 }
